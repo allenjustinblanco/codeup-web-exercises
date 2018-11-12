0417ac4524f3cb88d711ebcb8045a208c957a2e6
Justin <justin@Justins-MacBook-Pro.local>
2018-11-12 16:58:10 -0600

    updated external js and placed into js folder, end of day 1 week 2

--- js/external.js
+++ js/external.js
@@ -1,4 +1,5 @@
 console.log('Hello from external Javascript');
 alert('Welcome to my Website!');
+var userInput = prompt('What is your favorite color?');
 prompt('What is your favorite color?');
-alert( ' is my favorite color too!');
+alert( userInput + ' is my favorite color too!');
