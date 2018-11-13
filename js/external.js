5a0d2a838deb0e16e9b80be3efc068aa555af671
Justin <justin@Justins-MacBook-Pro.local>
2018-11-13 10:59:21 -0600

    fixed all errors in order-pizza, finishing external js, messed with codeup design and bootstrap more

--- js/external.js
+++ js/external.js
@@ -1,5 +1,5 @@
 console.log('Hello from external Javascript');
 alert('Welcome to my Website!');
-var userInput = prompt('What is your favorite color?');
+var userInput = prompt();
 prompt('What is your favorite color?');
 alert( userInput + ' is my favorite color too!');
